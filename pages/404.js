7169fbf9d45b2fb1ecbbc68d367980642e061062
leonardo.santos <leonardo.santos@ao3tech.com>
2022-01-26 21:37:30 -0300

    page 404

--- pages/404.js
+++ pages/404.js
@@ -1,28 +1,43 @@
-import { Box } from "@skynexui/components";
-import AppConfig from '../config.json';
+import { Box, Text } from "@skynexui/components";
+import AppConfig from "../config.json";
 
 export default function NotFoundPage() {
-    return (
-        <Box styleSheet={{
-            display: 'flex',
-            backgroundColor: AppConfig.theme.colors.neutrals[500],
-            backgroundImage: "url('https://pbs.twimg.com/ext_tw_video_thumb/1273056357357404160/pu/img/JVA1QJ8gpj6mVrrP.jpg')",
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundBlendMode: "multiply",
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-            <Box
-                styleSheet={{
-                    width: {
-                        md: '50%'
-                    },
-                    backgroundColor: AppConfig.theme.colors.neutrals[200]
-                }}
-            >
-                box
-            </Box>
-        </Box>
-    );
+	return (
+		<Box
+			styleSheet={{
+				display: "flex",
+				backgroundColor: AppConfig.theme.colors.neutrals[500],
+				backgroundImage:
+					"url('https://pbs.twimg.com/ext_tw_video_thumb/1273056357357404160/pu/img/JVA1QJ8gpj6mVrrP.jpg')",
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+				backgroundBlendMode: "multiply",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Box
+				styleSheet={{
+					width: {
+						md: "50%",
+					},
+					height: {
+						md: "50%",
+					},
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+                    textAlign: 'center',
+					backgroundColor: AppConfig.theme.colors.neutrals[200],
+				}}
+			>
+				<Text>
+					Parece que vcê aterricou junto com a Clark na terra
+					inabitavel!
+				</Text>
+				<Text styleSheet={{marginTop: 15}}>Volte de onde veio e ache seu caminho</Text>
+			</Box>
+		</Box>
+	);
 }

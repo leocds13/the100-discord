50721c08a61fd6c4ed051640514bbbe4d5b54c1d
leonardo.santos <leonardo.santos@ao3tech.com>
2022-01-30 15:15:03 -0300

    Correção next.config

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM2NDE1NywiZXhwIjoxOTU4OTQwMTU3fQ.jjpyeSUIOoRcqegY0lUSRtitd_CW3WO-3oX35m6Jw6s",
+        SUPABASE_URL: "https://tzqirlkczivpyhwghbzw.supabase.co"
+    }
+}
